7d760c4068c0c14f9c1f3bf6f0d306de8fe3061f
gamefreakoneone <amogh@outlook.com>
2020-08-14 17:21:58 +0530

    upload

--- BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,42 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    //Welcoming the layer
+    PrintLine(TEXT("Welcome to bull cow game"));
+    PrintLine(TEXT("Guess the four letter word.")); //Magic number remove
+    PrintLine(TEXT("Press enter to continue."));
+    
+    //Setting Up Game
+    HiddenWord=TEXT("halo"); // Set the HiddenWord
+
+    //Set Lives
+
+    //Prompt Player for guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
 
+    //Checking Player guess
+
+     //Move outside the function
+    PrintLine(Input);
+    if(Input==HiddenWord){
+        PrintLine(TEXT("You win!"));
+    }else{
+        PrintLine(TEXT("You Lose!"));
+    }
+    //remove lives
+    //Check if Isogram
+    //Prompt to guess again
+    //Check rigt number of characters
+    //Prompt again
+    // Check if Lives are reater than 0
+    //if Yes GuessAgain
+    //Show lives left
+    //if No Show Gameover and HiddenWord
+    //Prompt to Play Again. Press Enter to play again
+    //Check User input
 }
